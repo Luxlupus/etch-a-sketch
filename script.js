6eae9e522f003752c2581078d5529c7012632193
Luxlupus <ispiljak@gmail.com>
2023-12-14 21:00:03 +0100

    Updated style, integrated interactive functions.

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
-let gridSize = 0;
-
-//ask user for the desired size of grid with upper limit of 64
-function getGridSize() {
-    gridSize = prompt("Enter sketch grid size: ?");
-    if (gridSize > 64)
-        getGridSize();
-    return gridSize; }
-
-//draw the sketch grid
-function drawGrid(gridSize) {
-    const grid = document.querySelector("#grid");
-    grid.addEventListener('click',() => {
-        for (let i = 0; i < gridSize; i++){
+function drawGrid() {
+        
+        const gridInputRow = document.getElementById("numberBoxFirst").value;
+        const gridInputColumn = gridInputRow;
+        
+        for (let i = 0; i < gridInputRow; i++){
             const gridRow = document.createElement("div");
             gridRow.classList.toggle("gridRow");
+            gridRow.setAttribute("id", "row"+i);
             grid.appendChild(gridRow); 
-    
-                for (let j = 0; j < gridSize; j++) {
+                for (let j = 0; j < gridInputColumn; j++) {
                     const gridColumn = document.createElement("div");
                     gridColumn.classList.toggle("gridColumn");
-                    gridRow.appendChild(gridColumn); } 
-        } })
+                    gridColumn.setAttribute("id","column"+j);
+                    gridRow.appendChild(gridColumn); }        
+            }
 }
 
-getGridSize();
-drawGrid(gridSize);
+function submitForm(event) {
+        event.preventDefault();
+        const submit = document.getElementById("submitBtn");
+        const numberBoxFirst = document.getElementById("numberBoxFirst");
+        const numberBoxSecond = document.getElementById("numberBoxSecond");
+        submit.disabled = true;
+        numberBoxFirst.disabled = true;
+        drawGrid();
+        
+}
+
+function color(event) {
+    const gridColumn = document.getElementsByClassName("gridColumn");
+    for (const element of gridColumn) {
+    element.addEventListener('click', () => {
+            element.classList.add("color");
+            clickAndDrag();
+        })
+    }
+}
+
+function rmColor(event) {
+        const gridColumn = document.getElementsByClassName("gridColumn");
+        for (const element of gridColumn) {
+        element.addEventListener('dblclick', () => {
+            element.classList.add('noColor');
+            element.classList.remove("color")
+            })
+        }
+}
+
+window. onload = function() { document. getElementById("numberBoxFirst"). focus(); }
+
+const form = document.getElementById("form");
+form.addEventListener('submit', submitForm);
+form.addEventListener('reset', () => { window.parent.location = window.parent.location.href; });
+
+const grid = document.getElementById("grid");
+//let gridClicked = true;
+grid.addEventListener('mouseenter', function (event) {
+    color();
+    rmColor();
+    });
+
+
+
+
